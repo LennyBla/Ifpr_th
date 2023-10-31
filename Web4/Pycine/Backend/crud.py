74ed8795f9e33648a5911c8384b702b2beb24335
Thais Florentin <84157862+LennyBla@users.noreply.github.com>
2023-10-31 19:24:20 -0300

    Update crud.py

--- Web4/Pycine/Backend/crud.py
+++ Web4/Pycine/Backend/crud.py
@@ -1,26 +1,29 @@
+from fastapi import Depends
 from sqlalchemy.orm import Session
 import models, schemas
 
-
 def get_user(db: Session, user_id: int):
+    #SELECT * from users where id = user_id
     return db.query(models.User).filter(models.User.id == user_id).first()
 
+def get_user_by_name(db: Session, name: str):
+    return db.query(models.User).filter(models.User.name == name).first()
 
 def get_user_by_email(db: Session, email: str):
     return db.query(models.User).filter(models.User.email == email).first()
-
 
 def get_users(db: Session, skip: int = 0, limit: int = 100):
     return db.query(models.User).offset(skip).limit(limit).all()
 
 def create_user(db: Session, user: schemas.UserCreate):
     fake_hashed_password = user.password + "notreallyhashed"
-    db_user = models.User(email=user.email, name=user.name, hashed_password=fake_hashed_password)
+    db_user = models.User(name=user.name, email=user.email, hashed_password=fake_hashed_password)
     db.add(db_user)
     db.commit()
     db.refresh(db_user)
     return db_user
 
+  
 def update_user(db: Session, user_id: int, user: schemas.UserCreate):
     db_user = db.query(models.User).filter(models.User.id == user_id).first()
     db_user.name = user.name
@@ -28,19 +31,18 @@ def update_user(db: Session, user_id: int, user: schemas.UserCreate):
     db.commit()
     db.refresh(db_user)
     return db_user
-
 def delete_user(db: Session, user_id: int):
-    db.query(models.User).filter(models.User.id == user_id).delete()
-    db.commit()
-    return user_id
-
-def delete_favorite(db: Session, user_id: int, favorite_id: int):
-    user = db.query(models.User).filter(models.User.id == user_id).first()
-    favorite = db.query(models.Favorite).filter(models.Favorite.id == favorite_id).first()
-    
-    if favorite in user.favorites:
-        user.favorites.remove(favorite)
-        db.delete(favorite)
+    user_to_delete = db.query(models.User).filter(models.User.id == user_id).first()
+    if user_to_delete:
+        db.delete(user_to_delete)
         db.commit()
-    
-    return favorite_id
+        return user_to_delete
+    return None 
+
+#update
+def favoritar(db:Session, favorito: schemas.Favoritos):
+    db_favorito= models.Favorito_movie(tmdb_id=favorito.tmdb_id)
+    db.add(db_favorito)
+    db.commit()
+    db.refresh(db_favorito)
+    return db_favorito
