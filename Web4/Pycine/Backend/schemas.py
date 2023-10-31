7b6febe5ccc33cfdd3264a5a48517360a66fd172
Thais Florentin <84157862+LennyBla@users.noreply.github.com>
2023-10-31 19:25:59 -0300

    Update schemas.py

--- Web4/Pycine/Backend/schemas.py
+++ Web4/Pycine/Backend/schemas.py
@@ -1,5 +1,24 @@
 from pydantic import BaseModel
 
+class MovieBase(BaseModel):
+    tmdb_id: str
+    title: str
+
+class MovieCreate(MovieBase):
+    pass
+
+class Movie(MovieBase):
+    id: int
+
+class FavoritosBase(BaseModel):
+    tmdb_id: str
+
+class FavoritosCreate(FavoritosBase):
+    pass
+
+class Favoritos(FavoritosBase):
+    id: int
+
 class UserBase(BaseModel):
     name: str
     email: str
