35bcc074e350eae9f226f2ba61df39339128c14c
Thais Florentin <84157862+LennyBla@users.noreply.github.com>
2023-11-07 14:06:33 -0300

    Update Favoritos.svelte

--- Web4/Pycine/Forntend/src/routes/Favoritos.svelte
+++ Web4/Pycine/Forntend/src/routes/Favoritos.svelte
@@ -1,41 +1,48 @@
 <script>
     let promise = getFavorites();
-    async function getFavorites(){
-        const res = fetch('http://localhost:8000/favorites')
-        const text = await res.json();
-        if(res.ok){
-            return text;
-        }else{
-            throw new Error(text);
+    
+    async function getFavorites() {
+        // Faça a solicitação para o serviço TMDB
+        const apiKey = '6f77cb8794e999fed44476c8b3303723'; // Substitua por sua chave de API TMDB
+        const tmdbURL = `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}`;
+        
+        const res = await fetch(tmdbURL);
+        
+        if (res.ok) {
+            const data = await res.json();
+            return data.results; // Supondo que os favoritos estejam nos resultados
+        } else {
+            throw new Error('Falha ao obter dados do TMDB');
         }
     }
 
-    function handleClick(){
+    function handleClick() {
         promise = getFavorites();
     }
 </script>
+
 <div class="content FlexCenter">
     <button on:click={handleClick}> Get Favorites </button>
     {#await promise}
         <p>...waiting</p>
     {:then favorites}
         <h1>Lista de Favoritos</h1>
-    {#each favorites as favorite}
-        <div>
-            <p>
-                <span>ID: </span>
-                {favorite.id}
-            </p>
+        {#each favorites as favorite}
+            <div>
+                <p>
+                    <span>ID: </span>
+                    {favorite.id}
+                </p>
 
-            <p>
-                <span>Title: </span>
-                {favorite.title}
-            </p>
-        </div>
-    {/each}
+                <p>
+                    <span>Title: </span>
+                    {favorite.title}
+                </p>
+            </div>
+        {/each}
     {/await}
 </div>
 
-  <style>
+<style>
 
-  </style>
+</style>
